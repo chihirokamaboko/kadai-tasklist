63c6471e6ebfe4c953619037321daab55574ab54
chihiro.kameyama <chihirokamaboko@gmail.com>
2020-11-20 00:42:11 +0000

    clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   
  
-  root to:'tasks#index'
+  get 'toppages/index'
+  root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:new, :create ]
-  resources :tasks, only:[:new, :create, :edit, :destroy, :index, :show, :update]
+  resources :users, only: [:new, :create]
+  resources :tasks
 end
